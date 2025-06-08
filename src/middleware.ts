2e97375c1614e3e8acaf06034070706bff7be728
Dhavidy Pires <p266dd@gmail.com>
2025-06-08 21:26:11 +0900

    Created the manifest file.

--- src/middleware.ts
+++ src/middleware.ts
@@ -30,5 +30,7 @@ export default async function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|.*\\.png|.*\\.ico$).*)"],
+  matcher: [
+    "/((?!api|_next/static|_next/image|manifest\\.webmanifest|.*\\.png|.*\\.ico$).*)",
+  ],
 };
